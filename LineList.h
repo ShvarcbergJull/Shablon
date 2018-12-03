f2eec9ebe86c5f62839d39062421b358ea2e7318
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-12-03 16:35:13 +0800

    Update LineList.h

--- LineList.h
+++ LineList.h
@@ -1,16 +1,28 @@
 #pragma once
 #include <iostream>
 
-//using namespace std;
+using namespace std;
+
+class LineListException {};
 
 template <class T> class LineListElem
 {
 	T data;
 	LineListElem* next;
 public:
-	LineListElem(const T& adata, LineListElem* anext);
-	const T& getData() const;
-	LineListElem* getNext();
+	LineListElem(const T& adata, LineListElem* anext)
+	{
+		data = adata;
+		next = anext;
+	}
+	const T& getData() const
+	{
+		return data;
+	}
+	LineListElem* getNext()
+	{
+		return next;
+	}
 	template <class T>	friend class LineList; 
 };
 
@@ -18,33 +30,75 @@ template <class T> class LineList
 {
 	LineListElem<T>* start;
 	LineList(const LineList& list);
-	LineList& operator =(const LineList& list);
+	LineList& operator =(const LineList& list)
+	{
+		start = list.start;
+	}
 
 public:
-	LineList();
-	~LineList();
-	LineListElem<T>* getStart();
-	void deleteFirst();
-	void deleteAfter(LineListElem<T>* ptr);
-	void insertFirst(const T& data);
-	void insertAfter(LineListElem<T>* ptr, const T& data);
-
-	template <class T> friend std::ostream &operator<<(ostream &out, const LineList<T> &list);
-};
-
-class LineListException{};
-
-/*
-template <class T> ostream& operator <<(ostream& out, LineList<T>& list)
-{
-	LineListElem<T>* ptr = list.start;
-	if (!ptr)
-		cout << "EMPTY" << endl;
-	else
-		while (ptr)
+	LineList()
+	{
+		start = 0;
+	}
+	~LineList()
+	{
+		while (start)
+			deleteFirst();
+	}
+	LineListElem<T>* getStart()
+	{
+		return start;
+	}
+	void deleteFirst()
+	{
+		if (start)
 		{
-			out << ptr->getData() << ' ';
-			ptr = ptr->getNext();
+			LineListElem<T>* temp = start->next;
+			delete start;
+			start = temp;
 		}
-	return out;
-}*/
+		else
+			throw LineListException();
+	}
+	void deleteAfter(LineListElem<T>* ptr)
+	{
+		if (ptr && ptr->next)
+		{
+			LineListElem<T>* temp = ptr->next;
+			ptr->next = temp->next;
+			delete temp;
+		}
+		else
+			throw LineListException();
+
+	}
+	void insertFirst(const T& data)
+	{
+		LineListElem<T>* second = start;
+		start = new LineListElem<T>(data, second);
+	}
+	void insertAfter(LineListElem<T>* ptr, const T& data)
+	{
+		if (ptr)
+		{
+			LineListElem<T>* temp = ptr->next;
+			ptr->next = new LineListElem<T>(data, temp);
+		}
+		else
+			throw LineListException();
+	}
+
+	template <class T> friend ostream &operator<<(ostream &out, const LineList<T> &list)
+	{
+		LineListElem<T>* ptr = list.start;
+		if (!ptr)
+			cout << "EMPTY" << endl;
+		else
+			while (ptr)
+			{
+				out << ptr->getData() << ' ';
+				ptr = ptr->getNext();
+			}
+		return out;
+	}
+};
