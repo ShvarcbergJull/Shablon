531e6c82add42731214ef061636d25f464afb688
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-12-07 00:25:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ int main()
 	list.insertAfter(ptr->getNext(), 12);
 	list.insertFirst(7);
 	cout << "Шаг 1: " << list << endl;
+	cout << "Шаг 2: " << list.getPointer(15) << endl;
+	cout << "Шаг 3: " << list.getPoint(1) << endl;
 	
 	system("pause");
 	return 0;
